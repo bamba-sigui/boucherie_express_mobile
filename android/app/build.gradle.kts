f2cbe05506909832b16dbfc22a64f6cd50f8ff8c
bamba-sigui <124349684+bamba-sigui@users.noreply.github.com>
2026-05-02 17:39:57 +0000

    save state

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -47,4 +48,5 @@ flutter {
 dependencies {
   // Import the Firebase BoM
   implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
